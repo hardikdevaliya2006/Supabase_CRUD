314d90bad760373762cfebea3ba045bdc90fbc11
hardikdevaliya2006] <hardikdevaliya2006@gmail.com>
2025-11-26 07:17:05 +0530

    fix: adjust Navbar styling and layout in Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,10 @@ import Navbar from "../components/Navbar";
 const Login = () => {
   return (
     <main className="h-screen bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] ">
-      <header className="bg-slate-800 flex justify-center items-center h-[5vh]">
+      <header className="bg-slate-800 flex justify-center items-center h-[6vh]">
         <Navbar></Navbar>
       </header>
-      <section className="flex h-[90vh] items-center justify-center">
+      <section className="flex h-[88vh] items-center justify-center">
         <LoginForm></LoginForm>
       </section>
       <footer></footer>
